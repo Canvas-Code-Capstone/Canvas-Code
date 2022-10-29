6fd1e9dfab115b47ebea684eb5994b6c45e45094
Mark Taylor <taylor26@seattleu.edu>
2022-10-28 18:49:36 -0700

    Content script now sends alerts depending on which webpage user is on.
    Cannot inject JS into canvas without authroization.

--- frontend/canvas-chrome-ext/src/scripts/Content.js
+++ frontend/canvas-chrome-ext/src/scripts/Content.js
@@ -1,6 +1,30 @@
 
 console.log("Chrome ext ready");
 
+const site = window.location.hostname;
+
+alert("Canvas-Code JS has been injected into: " + site);
+
+
+function read_only_preview_SU(tag, attr_tag, attr_name, value){
+    const code_preview = document.createElement(tag);
+    code_preview.setAttribute(attr_tag, attr_name)
+    code_preview.innerHTML = value;
+    document.body.append(code_preview);
+}
+
+if (site.includes("https://seattleu.instructure.com/")) {
+    alert("inside SU");
+
+    read_only_preview_SU("div", "id", "js-read-only-preview", "custom element 1" );
+}
+
+if(site.includes("google.com")){
+
+    alert("inside google");
+    read_only_preview_SU("div", "id", "js-read-only-preview", "custom element 1" );
+}
+
 
 (function () {
 
@@ -10,6 +34,7 @@ console.log("Chrome ext ready");
         btn.id = "submit_compile_code";
         btn.type = "submit";
 
+        //find where to add upload button
         const el = document.querySelector(".FPdoLc.lJ9FBc center");
 
         console.log(el);
