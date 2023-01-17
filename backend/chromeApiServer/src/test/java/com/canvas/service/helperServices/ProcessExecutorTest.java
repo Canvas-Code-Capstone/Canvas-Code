20212bd4d1b8cb0cf51a020acfe67377ca41de6c
Omid Shamoil <oshamoil.dev@gmail.com>
2023-01-16 18:49:58 -0800

    ProcessExecutor unit test

--- backend/chromeApiServer/src/test/java/com/canvas/service/helperServices/ProcessExecutorTest.java
+++ backend/chromeApiServer/src/test/java/com/canvas/service/helperServices/ProcessExecutorTest.java
@@ -1,5 +1,50 @@
 package com.canvas.service.helperServices;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 class ProcessExecutorTest {
 
+    @Test
+    public void testExecuteProcess() throws IOException {
+        File file = new File("fooDirectory");
+        file.mkdirs();
+
+        String[] executeCommands = {"ls"};
+        ProcessExecutor processExecutor = new ProcessExecutor(executeCommands, "fooDirectory");
+
+        Assertions.assertTrue(processExecutor.executeProcess());
+
+        file.delete();
+    }
+
+    @Test
+    public void testExecuteProcess_throwsException() throws IOException {
+        String[] executeCommands = {"ls"};
+        ProcessExecutor processExecutor = new ProcessExecutor(executeCommands, "fooDirectory");
+
+        Assertions.assertFalse(processExecutor.executeProcess());
+    }
+
+    @Test
+    public void testGetProcessOutput() {
+        File file = new File("fooDirectory");
+        file.mkdirs();
+
+        String[] executeCommands = {"sh", "echo \"hello\""};
+        ProcessExecutor processExecutor = new ProcessExecutor(executeCommands, "fooDirectory");
+
+        processExecutor.executeProcess();
+        Assertions.assertEquals(
+                "sh: echo \"hello\": No such file or directory\n",
+                processExecutor.getProcessOutput()
+        );
+        file.delete();
+        // Probably a better way to do this test
+    }
 }
